c58f5a40fb2e27d77feac77cc7a6765778f2925e
Eugene Khristo <marteau9292@gmail.com>
2025-06-26 10:51:52 +0300

    First Exercise FIXED

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,47 +8,48 @@ import VisuallyHidden from '../VisuallyHidden';
 const SIZES = {
   small: {
     '--height': '8px',
-    '--borderRadius': '4px',
+    '--radius': '4px',
     '--padding': 0,
-    '--innerBorderRadius': 0,
   },
   medium: {
     '--height': '12px',
-    '--borderRadius': '4px',
+    '--radius': '4px',
     '--padding': 0,
-    '--innerBorderRadius': 0,
   },
   large: {
     '--height': '24px',
-    '--borderRadius': '8px',
+    '--radius': '8px',
     '--padding': '4px',
-    '--innerBorderRadius': '4px',
   },
 };
 
 const ProgressBar = ({ value, size }) => {
   return (
-    <Wrapper style={SIZES[size]}>
-      <Progress style={SIZES[size]} value={value} />
+    <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={SIZES[size]}>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar style={{ ...SIZES[size], '--width': value + '%' }} value={value} />
+      </BarWrapper>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  width: 370px;
-  height: var(--height);
   background-color: ${COLORS.transparentGray15};
-  border-radius: var(--borderRadius);
+  border-radius: var(--radius);
   box-shadow: 0px 2px 4px 0px ${COLORS.transparentGray35} inset;
-  overflow: hidden;
   padding: var(--padding);
 `;
 
-const Progress = styled.div`
-  width: ${(props) => props.value + '%'};
-  height: 100%;
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+`;
+
+const Bar = styled.div`
+  height: var(--height);
+  width: var(--width);
   background-color: ${COLORS.primary};
-  border-radius: var(--innerBorderRadius);
 `;
 
 export default ProgressBar;
